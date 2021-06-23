4c1048de97fe1a41c3e2015a6cf2dc4398882011
estelleh <estelleh@kth.se>
2021-06-24 01:34:05 +0200

    Done

---
+++ ThirtyDiceGame/app/src/main/java/java/se/umu/mobileapp/eshu0006/thirtydicegame/MainActivity.java
@@ -0,0 +1,14 @@
+package java.se.umu.mobileapp.eshu0006.thirtydicegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
